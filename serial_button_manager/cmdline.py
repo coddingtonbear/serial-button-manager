ed79875b333a533af77bff17ef9bce69ce889974
Adam Coddington <me@adamcoddington.net>
2015-11-14 15:22:54 -0800

    Consume only a single argument.

--- serial_button_manager/cmdline.py
+++ serial_button_manager/cmdline.py
@@ -13,8 +13,8 @@ def main(args=None):
         args = sys.argv[1:]
 
     parser = ArgumentParser()
-    parser.add_argument('serial_device', nargs=1)
-    parser.add_argument('service_name', nargs=1)
+    parser.add_argument('serial_device', nargs='?')
+    parser.add_argument('service_name', nargs='?')
     parser.add_argument('--loglevel', default='DEBUG')
 
     options = parser.parse_args(args)
