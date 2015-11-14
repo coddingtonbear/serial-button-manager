19ed0148a8aa0dcefac5ed475ea66cc4e408aad8
Adam Coddington <me@adamcoddington.net>
2015-11-14 14:44:37 -0800

    And so it begins.

---
+++ serial_button_manager/cmdline.py
@@ -0,0 +1,25 @@
+from argparse import ArgumentParser
+import logging
+import sys
+
+from .manager import SerialButtonManager
+
+
+logger = logging.getLogger(__name__)
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv
+
+    parser = ArgumentParser()
+    parser.add_argument('serial_device', nargs=1)
+    parser.add_argument('service_name', nargs=1)
+    parser.add_argument('--loglevel', default='DEBUG')
+
+    options = parser.parse_args(args)
+
+    logger.basicConfig(level=logging.getLevelName(options.loglevel))
+
+    manager = SerialButtonManager(options.serial_device, options.service_name)
+    manager.run()
